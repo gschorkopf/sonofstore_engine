88318468c3a2eee00bcb918748a129651630eb6d
Geoff Schorkopf <gschorkopf@gmail.com>
2013-04-16 12:26:53 -0600

    Added index products functionality to controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,21 +1,28 @@
 class ProductsController < ApplicationController
 
   def index
-    if current_store && current_store.pending?
-      redirect_to root_path,
-                      alert: "The store you are looking for does not exist."
-      return
-    elsif current_store.active == false && current_store.approved?
-      redirect_to root_path,
-                alert: "#{current_store.name} is currently down for maintenance."
-      return
-    elsif current_store.approved?
-      @products = current_store.products
-      @categories = current_store.categories
+    if current_store
+      if current_store.pending?
+        redirect_to root_path,
+                        alert: "The store you are looking for does not exist."
+        return
+      elsif current_store.active == false && current_store.approved?
+        redirect_to root_path,
+                  alert: "#{current_store.name} is currently down for maintenance."
+        return
+      elsif current_store.approved?
+        @products = current_store.products
+        @categories = current_store.categories
+      else
+        # 404
+        redirect_to root_path,
+                        alert: "The store you are looking for does not exist."
+        return
+      end
     else
       # 404
       redirect_to root_path,
-                      alert: "The store you are looking for does not exist."
+                        alert: "The store you are looking for does not exist."
       return
     end
   end
