1cfd368928c2799de9892d103b827af8782f0f54
Dan Mee <danmee10@gmail.com>
2013-04-16 22:53:01 -0600

    added purchas info forms to profile page

--- app/controllers/billing_addresses_controller.rb
+++ app/controllers/billing_addresses_controller.rb
@@ -39,12 +39,18 @@ class BillingAddressesController < ApplicationController
   def update
     @customer = current_user.customer
     @billing_address = BillingAddress.find_by_customer_id(@customer.id)
+    params.delete("utf8")
+    params.delete("_method")
+    params.delete("authenticity_token")
+    params.delete("controller")
+    params.delete("commit")
+    params.delete("action")
 
-    if @billing_address.update_attributes(params[:billing_address])
+    if @billing_address.update_attributes(params)
       redirect_to profile_path,
         :notice  => "Successfully updated billing address."
     else
-      render :action => 'edit', :notice  => "Update failed."
+      rendirect_to profile_path, :notice  => "Update failed."
     end
   end
 end
