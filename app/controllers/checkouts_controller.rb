79915c8f86f9e38ecd43f752373f4db4593bdebb
Dan Mee <danmee10@gmail.com>
2013-04-17 15:25:02 -0600

    minor

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -11,6 +11,6 @@ class CheckoutsController < ApplicationController
     @customer.billing_address_id = @checkout.billing
     @customer.credit_card_id = @checkout.credit_card
     @customer.save
-    redirect_to new_order_path(current_store, @customer.id)
+    redirect_to store_new_order_path(current_store, @customer.id)
   end
 end
