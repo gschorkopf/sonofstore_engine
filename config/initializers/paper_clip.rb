0a9ba9e2506e70022c794ff85f710d56f776fb1c
lalalainexd <lalalainexd@gmail.com>
2013-04-25 13:34:16 -0600

    fixed photo issue with aws

--- config/initializers/paper_clip.rb
+++ config/initializers/paper_clip.rb
@@ -1,1 +1,2 @@
-Paperclip::Attachment.default_options[:url] = 'http://s3.amazonaws.com/ballmer_dose'
+#Paperclip::Attachment.default_options[:url] = 'http://s3.amazonaws.com/ballmer_dose'
+#Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
--- db/seeds.rb
+++ db/seeds.rb
@@ -38,7 +38,6 @@ add_photo_to_product(hv1, "hv1")
 heroes_and_villains.products << hv1
 heroes.products << hv1
 males.products << hv1
-hv1.save!
 
 hv2 = Product.new(title: "Iron Patriot",
                   description: "From Iron Man 3",
@@ -48,7 +47,6 @@ add_photo_to_product(hv2, "hv2")
 heroes_and_villains.products << hv2
 heroes.products << hv2
 males.products << hv2
-hv2.save!
 
 hv3 = Product.new(title: "Domo Superman",
                   description: "domoooo",
@@ -58,7 +56,6 @@ add_photo_to_product(hv3, "hv3")
 heroes_and_villains.products << hv3
 heroes.products << hv3
 males.products << hv3
-hv2.save!
 
 hv4 = Product.new(title: "Domo Batman",
                   description: "domooo",
@@ -68,7 +65,6 @@ add_photo_to_product(hv4, "hv4")
 heroes_and_villains.products << hv4
 heroes.products << hv4
 males.products << hv4
-hv2.save!
 
 hv5 = Product.new(title: "Robin",
                   description: "robin",
@@ -78,7 +74,6 @@ add_photo_to_product(hv5, "hv5")
 heroes_and_villains.products << hv5
 heroes.products << hv5
 males.products << hv5
-hv2.save!
 
 hv6 = Product.new(title: "Wonder Woman",
                   description: "wondeerrr",
@@ -88,7 +83,6 @@ add_photo_to_product(hv6, "hv6")
 heroes_and_villains.products << hv6
 heroes.products << hv6
 females.products << hv6
-hv2.save!
 
 hv7 = Product.new(title: "Dr. Doom",
                   description: "doooom",
@@ -98,7 +92,6 @@ add_photo_to_product(hv7, "hv7")
 heroes_and_villains.products << hv7
 villains.products << hv7
 males.products << hv7
-hv2.save!
 
 hv8 = Product.new(title: "Joker",
                   description: "haha",
@@ -108,7 +101,6 @@ add_photo_to_product(hv8, "hv8")
 heroes_and_villains.products << hv8
 villains.products << hv8
 males.products << hv8
-hv2.save!
 
 hv9 = Product.new(title: "Captain Hook",
                   description: "arrrrr",
@@ -118,7 +110,6 @@ add_photo_to_product(hv9, "hv9")
 heroes_and_villains.products << hv9
 villains.products << hv9
 males.products << hv9
-hv2.save!
 
 hv10 = Product.new(title: "Spiderman",
                   description: "piderman?",
@@ -128,7 +119,6 @@ add_photo_to_product(hv10, "hv10")
 heroes_and_villains.products << hv10
 heroes.products << hv10
 males.products << hv10
-hv2.save!
 
 hv11 = Product.new(title: "Deadpool",
                   description: "something er something",
@@ -138,7 +128,6 @@ add_photo_to_product(hv11, "hv11")
 heroes_and_villains.products << hv11
 villains.products << hv11
 males.products << hv11
-hv2.save!
 
 hv12 = Product.new(title: "Spiderman",
                   description: "piderman?",
@@ -148,7 +137,6 @@ add_photo_to_product(hv12, "hv12")
 heroes_and_villains.products << hv12
 heroes.products << hv12
 males.products << hv12
-hv2.save!
 
 hv13 = Product.new(title: "Catwoman",
                   description: "Meow",
@@ -158,7 +146,6 @@ add_photo_to_product(hv13, "hv13")
 heroes_and_villains.products << hv13
 villains.products << hv13
 females.products << hv13
-hv2.save!
 
 hv14 = Product.new(title: "Greenlantern",
                   description: "zomg so OP",
@@ -168,7 +155,6 @@ add_photo_to_product(hv14, "hv14")
 heroes_and_villains.products << hv14
 heroes.products << hv14
 males.products << hv14
-hv2.save!
 
 hv15 = Product.new(title: "She-RA",
                   description: "RA RA RA",
@@ -178,7 +164,6 @@ add_photo_to_product(hv15, "hv15")
 heroes_and_villains.products << hv15
 heroes.products << hv15
 females.products << hv15
-hv2.save!
 
 hv16 = Product.new(title: "Beast",
                   description: "I'm da beeaaasstt",
@@ -188,7 +173,6 @@ add_photo_to_product(hv16, "hv16")
 heroes_and_villains.products << hv16
 heroes.products << hv16
 males.products << hv16
-hv2.save!
 
 hv17 = Product.new(title: "Pink Ranger",
                   description: "so pink",
@@ -198,7 +182,6 @@ add_photo_to_product(hv17, "hv17")
 heroes_and_villains.products << hv17
 heroes.products << hv17
 females.products << hv17
-hv2.save!
 
 hv18 = Product.new(title: "He-Man",
                   description: "HHAAAAAA",
@@ -208,7 +191,6 @@ add_photo_to_product(hv18, "hv18")
 heroes_and_villains.products << hv18
 heroes.products << hv18
 males.products << hv18
-hv2.save!
 
 hv19 = Product.new(title: "Penguin",
                   description: "quack?",
@@ -218,7 +200,6 @@ add_photo_to_product(hv19, "hv19")
 heroes_and_villains.products << hv19
 villains.products << hv19
 males.products << hv19
-hv2.save!
 
 hv20 = Product.new(title: "The Riddler",
                   description: "Riddles are better than jokes",
@@ -567,14 +548,15 @@ teaparty20 = Product.new(title: "The Riddler",
 add_photo_to_product(teaparty20, "teaparty20")
 tea_party.products << teaparty20
 pink.products << teaparty20
-# customer2 = Customer.create(full_name: "Jeff", email: "demoXX+jeff@jumpstartlab.com")
-# customer3 = Customer.create(full_name: "Steve Klabnik", email: "demoXX+steve@jumpstartlab.com")
-# customer4 = Customer.create(full_name: "Katrina", email: "demoXX+katrina@jumpstartlab.com")
 
-# user1 = User.create(password: "password", customer_id: customer1.id)
-# user2 = User.create(password: "password", display_name: "j3", customer_id: customer2.id)
-# user3 = User.create(password: "password", display_name: "SkrilleX", customer_id: customer3.id)
-# user4 = User.create(password: "password", display_name: "Norway", customer_id: customer4.id)
+customer2 = Customer.create!(full_name: "Jeff", email: "demoXX+jeff@jumpstartlab.com")
+customer3 = Customer.create!(full_name: "Steve Klabnik", email: "demoXX+steve@jumpstartlab.com")
+customer4 = Customer.create!(full_name: "Katrina", email: "demoXX+katrina@jumpstartlab.com")
+
+#user1 = User.create(password: "password", customer_id: customer1.id)
+user2 = User.create!(password: "password", display_name: "j3", customer_id: customer2.id)
+user3 = User.create!(password: "password", display_name: "SkrilleX", customer_id: customer3.id)
+user4 = User.create!(password: "password", display_name: "Norway", customer_id: customer4.id)
 
 # ur2 = UserRole.new
 # ur2.user_id = 2
